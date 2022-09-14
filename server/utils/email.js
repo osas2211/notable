a9dc53259a7f5e7990cd599637dcc99364c0a363
osaretin frank <osaretin.frank10@gmail.com>
2022-09-14 07:40:20 +0100

    added email service

---
+++ server/utils/email.js
@@ -0,0 +1,24 @@
+const nodemail = require("nodemailer")
+
+module.exports.sendEmail = async (to_mail, subject, body) => {
+  try {
+    const transporter = nodemail.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.MAIL,
+        pass: process.env.MAIL_PASSWORD,
+      },
+    })
+    await transporter.sendMail({
+      from: process.env.MAIL,
+      to: to_mail,
+      subject: subject,
+      html: body,
+      priority: "high",
+    })
+  } catch (error) {
+    console.log(error.message)
+  }
+
+  return transporter
+}
