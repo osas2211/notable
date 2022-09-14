97a8f2fbfdfb44672be341b1b94c9abd8fb9d5ce
osaretin frank <osaretin.frank10@gmail.com>
2022-09-14 07:49:31 +0100

    updated cloudinary

--- server/utils/cloudinary_setup.js
+++ server/utils/cloudinary_setup.js
@@ -3,9 +3,9 @@ const { CloudinaryStorage } = require("multer-storage-cloudinary")
 
 const cloudStorage = (folder_name) => {
   cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.API_KEY,
-    api_secret: process.env.API_SECRET,
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
     secure: true,
   })
 
